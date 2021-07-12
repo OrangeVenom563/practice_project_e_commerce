4dcadeff31dfe5824f9de4f933ce6197c0e65579
Lawrence J <lawrenceg563@gmail.com>
2021-07-12 14:22:37 +0530

    made menu items clickable and can navigate to respective links given in directory.component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const Hatspage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className='container'>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={Hatspage} />
+      </Switch>
+      {/* <HomePage/> */}
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ class Directory extends React.Component {
             sections: [{
                 title: 'HATS',
                 imageUrl: 'https://upgradedhome.com/wp-content/uploads/2021/03/shutterstock_1790483456.jpg',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'JACKETS',
                 imageUrl: 'https://media.wired.com/photos/606ce52941bf976945513469/191:100/w_2086,h_1092,c_limit/Gear-Cloudburst-Jacket---Mandarin-Front-square-grey-back.jpg',
-                id: 2
+                id: 2,
+                linkUrl:''
             },
             {
                 title: 'SNEAKERS',
                 imageUrl: 'https://media.gq.com/photos/5d93aa2c636d4800084025ae/16:9/pass/sneakers.jpg',
-                id: 3
+                id: 3,
+                linkUrl:''
             },
             {
                 title: 'WOMENS',
                 imageUrl: 'https://c4.wallpaperflare.com/wallpaper/828/961/289/photography-women-model-landscape-car-mercedes-benz-vintage-car-see-through-clothing-wallpaper-preview.jpg',
                 size:'large',
-                id: 4
+                id: 4,
+                linkUrl:''
             },
             {
                 title: 'MENS',
                 imageUrl: 'https://images.unsplash.com/photo-1550246140-29f40b909e5a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
                 size:'large',
-                id: 5
+                id: 5,
+                linkUrl:''
             }
             ]
         }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, size, id }) => 
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({ id, ...otherSectionProps }) => 
+                    <MenuItem {...otherSectionProps}/>
                 )}
             </div>
             )
