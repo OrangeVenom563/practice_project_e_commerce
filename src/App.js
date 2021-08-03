abb219f48d45f89c2ff2eea8a9aa3104e2e79ec4
Lawrence J <lawrenceg563@gmail.com>
2021-08-03 13:13:15 +0530

    added items list to cart

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(this.props)
     return (
       <div className="container">
         <Header />
@@ -41,7 +42,7 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/signin" render={()=>
-            this.props.setCurrentUser ? <Redirect to ='/'/> :
+            this.props.user ? <Redirect to='/'/> :
             <SignInSignUp/>
           } />
         </Switch>
